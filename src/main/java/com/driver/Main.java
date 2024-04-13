958592eaf1562ee98fe761fcc455615072da56b8
Your Name <wmsalmanayaj@gmail.com>
2024-04-13 12:55:55 +0530

    new code

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args){
 
         B obj = new B();
-        System.out.println(obj.meth());
+        obj.meth();
     }
     public static class A{
 
